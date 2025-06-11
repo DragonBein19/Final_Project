07bf9d9415f12d0229939e297cdab734f4a98b8d
GabrieleSiaud <gabrielesiaudvytyte9@gmail.com>
2025-06-11 21:56:26 +0300

    fixed index.js file

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 function NavigationBar() {
+  return <nav>Navigation here</nav>;
+}
+
+function SignUp() {
+  return <div>Sign Up Form</div>;
 }
 
 const domNode = document.getElementById('navigation');
-const root = createRoot(domNode);
-root.render(<NavigationBar />);
+if (domNode) {
+  const root = createRoot(domNode);
+  root.render(<NavigationBar />);
+}
 
 const rootNode = document.getElementById('root');
 if (rootNode) {
   const root = createRoot(rootNode);
   root.render(<SignUp />);
 }
-
